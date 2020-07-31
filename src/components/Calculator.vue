44281eef449531dc0769cee208bb3ec207f4c51b
Izabelle Araujo <izabellederrihu64@gmail.com>
2020-07-31 17:24:12 -0300

    changing v-for and adding css

--- src/components/Calculator.vue
+++ src/components/Calculator.vue
@@ -1,34 +1,29 @@
+<style src="./Calculator.css" scoped></style>
+
 <template>
  <section class="root">
-   <div class="calculator">
-     <div class="display">{{ display }}</div>
-     <div class="keyboard">
-       <div class="numbers">
-          <button v-for="(number, i) in numbers" :key="i" class="number-btn" @click='handleNumber(number)'> {{ number }} </button>
-       </div>
-       <div class="basicOperations">
-        <button class="operation-btn" 
-          @click='division()'>/</button>
-        <button class="operation-btn" 
-          @click='multiplication()'>X</button>
-        <button class="operation-btn" 
-          @click='subtraction()'>-</button>
-        <button class="operation-btn" 
-          @click='sum()'>+</button>
-
-        <button class="operation-btn"
-          @click='result()'>=</button>
-       </div>
-       <div class="controllers">
-          <button class="controller-btn clean" 
-            @click='clean()'>C</button>
-          <button class="controller-btn module"
-            @click='signal()'>+/-</button>
-          <button class="controller-btn percent"
-            @click="percent()">%</button>
-       </div>
-     </div>
-   </div>
+    <div class="calculator">
+      <div class="display">{{ display }}</div>
+      <button @click='clean()'>C</button>
+      <button @click='signal()'>+/-</button>
+      <button @click="percent()">%</button>  
+      <button @click='division()'>/</button>
+      <button @click='handleNumber("7")'>7</button>
+      <button @click='handleNumber("8")'>8</button>
+      <button @click='handleNumber("9")'>9</button>
+      <button class="operation" @click='multiplication()'>X</button>
+      <button @click='handleNumber("4")'>4</button>
+      <button @click='handleNumber("5")'>5</button>
+      <button @click='handleNumber("6")'>6</button>
+      <button class="operation" @click='subtraction()'>-</button>
+      <button @click='handleNumber("1")'>1</button>
+      <button @click='handleNumber("2")'>2</button>
+      <button @click='handleNumber("3")'>3</button>
+      <button class="operation" @click='sum()'>+</button>
+      <button class="zero-btn" @click='handleNumber("0")'>0</button>
+      <button @click='handleNumber(".")'>.</button>
+      <button class="operation" @click='result()'>=</button>
+    </div>
  </section>
 </template>
 
@@ -37,7 +32,6 @@ export default {
   name: 'Calculator',
   data () {
     return {
-      numbers: ['7', '8', '9', '4', '5', '6', '1', '2', '3', '0', '.'],
       number: '',
       oldNumber: '',
       operation: null,
@@ -46,10 +40,13 @@ export default {
   },
   computed: {
     display () {
-      return this.number
+      return this.number || '0'
     }
   },
   methods: {
+
+    // here I join the actual number with the next one clicked
+
     handleNumber (value) {
       if (this.opChoosed) {
         this.number = ''
@@ -58,29 +55,46 @@ export default {
 
       this.number = `${this.number}${value}`
     },
+
+    // here I set the new calculated value to Old number, so the user can continue calculating
     setValue() {
       this.oldNumber = this.number
       this.opChoosed = true
     },
+
+
+    // options
+
     clean () {
       this.number = ''
       this.oldNumber = ''
     },
     signal () {
-      this.number = this.number.charAt(0) === '-'
-        ? this.number.slice(1)
-        : `-${this.number}`
+      if (this.number) {
+        this.number = this.number.charAt(0) === '-'
+          ? this.number.slice(1)
+          : `-${this.number}`
+      }
     },
     percent () {
+      if (this.number) {
       this.number = `${parseFloat(this.number) / 100}`
+      }
     },
     result () {
-      this.number = `${this.operation(
-        parseFloat(this.oldNumber),
-        parseFloat(this.number)
-      )}`
-      this.oldNumber = ''
+      if (this.number && this.oldNumber) {
+          this.number = `${this.operation(
+          parseFloat(this.oldNumber),
+          parseFloat(this.number)
+        )}`
+        this.oldNumber = ''
+      }
     },
+
+
+    // basic operations 
+    // notice that I return an arrow function to our operation, then I can use it on result func
+
     division () {
       this.operation = (num1, num2) => num1 / num2
       this.setValue()
